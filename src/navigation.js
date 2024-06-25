2f78c32dcd1c0eaacf5337cfddc06fce9e179be0
wangjunfei <471612627@qq.com>
2024-06-25 10:54:01 +0800

    update header

--- src/navigation.js
+++ src/navigation.js
@@ -3,7 +3,7 @@ import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 export const headerData = {
   links: [
     {
-      text: 'ResearchTeam',
+      text: 'People',
       href: getPermalink('/team'),
     },
     {
